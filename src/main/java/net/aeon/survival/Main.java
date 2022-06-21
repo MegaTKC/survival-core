e6a93cbb5c69d13b9a5215779693b786976fd4ce
Fangoboyo <77935781+Fangoboyo@users.noreply.github.com>
2022-06-21 10:49:30 -0700

    added stuff

--- src/main/java/net/aeon/survival/Main.java
+++ src/main/java/net/aeon/survival/Main.java
@@ -1,5 +1,6 @@
 package net.aeon.survival;
 
+import net.aeon.survival.listeners.ArmorWearListeners;
 import org.bukkit.Bukkit;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -7,13 +8,13 @@ import org.bukkit.plugin.java.JavaPlugin;
 import net.aeon.survival.custom.boots.BoltBoots;
 import net.aeon.survival.listeners.MobListener;
 
-public class Main extends JavaPlugin implements Listener {
+public class Main extends JavaPlugin {
 	public void onEnable() {
-        Bukkit.getServer().getPluginManager().registerEvents(this, this);
         //Listeners
         getServer().getPluginManager().registerEvents(new MobListener(), this);
         // Custom Items
         getServer().getPluginManager().registerEvents(new BoltBoots(), this);
+        getServer().getPluginManager().registerEvents(new ArmorWearListeners(), this);
 	}
 	
 }
--- src/main/java/net/aeon/survival/listeners/MobListener.java
+++ src/main/java/net/aeon/survival/listeners/MobListener.java
@@ -10,8 +10,7 @@ public class MobListener implements Listener {
 	//Listen for sheep eating the grass, and when he eats it, the event gets cancelled.
 	@EventHandler
 	public void onSheepEatGrass(EntityChangeBlockEvent e) {
-		EntityType sheep = EntityType.SHEEP;
-		if (e.getEntity().getType() == sheep) {
+		if (e.getEntity().getType() == EntityType.SHEEP) {
 			if (e.getTo() == Material.DIRT) {
 				e.getBlock().setType(Material.GRASS);
 			}
